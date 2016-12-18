ddf3e01948c998408989ebd23dcb14caf3ac2f41
Jonathan Kohlhas <jonathan.kohlhas185@gmail.com>
2016-12-18 01:46:38 +0100

    I dont even know

---
+++ src/Camera.java
@@ -0,0 +1,117 @@
+import java.awt.AWTException;
+import java.awt.MouseInfo;
+import java.awt.Robot;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class Camera implements KeyListener, MouseListener {
+	public double xPos, yPos, xDir, yDir, xPlane, yPlane;
+	public boolean left, right, forward, back, focused;
+	public final double MOVE_SPEED = .08;
+	public final double ROTATION_SPEED = .045;
+	Robot rob = null;
+
+	public Camera(double xPos, double yPos, double xDir, double yDir, double xPlane, double yPlane) {
+		this.xPos = xPos;
+		this.yPos = yPos;
+		this.xDir = xDir;
+		this.yDir = yDir;
+		this.xPlane = xPlane;
+		this.yPlane = yPlane;
+		focused = true;
+		try {
+			rob = new Robot();
+		} catch (AWTException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void update(int[][] map) {
+		if (focused) {
+			int mousex = MouseInfo.getPointerInfo().getLocation().x - 200;
+			rob.mouseMove(200, 200);
+			double oldxDir = xDir;
+			xDir = xDir * Math.cos(ROTATION_SPEED * mousex) - yDir * Math.sin(ROTATION_SPEED * mousex);
+			yDir = oldxDir * Math.sin(ROTATION_SPEED * mousex) + yDir * Math.cos(ROTATION_SPEED * mousex);
+			double oldxPlane = xPlane;
+			xPlane = xPlane * Math.cos(ROTATION_SPEED * mousex) - yPlane * Math.sin(ROTATION_SPEED * mousex);
+			yPlane = oldxPlane * Math.sin(ROTATION_SPEED * mousex) + yPlane * Math.cos(ROTATION_SPEED * mousex);
+			if (forward) {
+				if (map[(int) (xPos + xDir * MOVE_SPEED)][(int) yPos] == 0) {
+					xPos += xDir * MOVE_SPEED;
+				}
+				if (map[(int) xPos][(int) (yPos + yDir * MOVE_SPEED)] == 0)
+					yPos += yDir * MOVE_SPEED;
+			}
+			if (back) {
+				if (map[(int) (xPos - xDir * MOVE_SPEED)][(int) yPos] == 0)
+					xPos -= xDir * MOVE_SPEED;
+				if (map[(int) xPos][(int) (yPos - yDir * MOVE_SPEED)] == 0)
+					yPos -= yDir * MOVE_SPEED;
+			}
+		}
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		focused = true;
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+			focused = false;
+		} else if (e.getKeyCode() == KeyEvent.VK_W) {
+			forward = true;
+		} else if (e.getKeyCode() == KeyEvent.VK_S) {
+			back = true;
+		} else if (e.getKeyCode() == KeyEvent.VK_D) {
+			left = true;
+		} else if (e.getKeyCode() == KeyEvent.VK_A) {
+			right = true;
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_W) {
+			forward = false;
+		} else if (e.getKeyCode() == KeyEvent.VK_S) {
+			back = false;
+		} else if (e.getKeyCode() == KeyEvent.VK_D) {
+			left = false;
+		} else if (e.getKeyCode() == KeyEvent.VK_A) {
+			right = false;
+		}
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+
+	}
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,96 @@
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
 
-public class Main {
+import javax.swing.JFrame;
+
+public class Main extends JFrame implements Runnable {
+	boolean running;
+	BufferedImage image;
+	int[][] world = new int[][] { { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+								  { 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1 }, 
+								  { 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1 },
+								  { 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1 }, 
+								  { 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1 },
+								  { 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1 }, 
+								  { 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1 },
+								  { 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1 }, 
+								  { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1 },
+								  { 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1 }, 
+								  { 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1 },
+								  { 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1 }, 
+								  { 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1 },
+								  { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 }, 
+								  { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 } };
+	Thread thread;
+	Camera cam;
+	boolean focused = true;
+
+	public Main() {
+		thread = new Thread(this);
+		image = new BufferedImage(640, 480, BufferedImage.TYPE_INT_RGB);
+		cam = new Camera(3,3,1,0,0,0.7);
+		addKeyListener(cam);
+		addMouseListener(cam);
+		setSize(640, 480);
+		setResizable(false);
+		setLocation(new Point(50,50));
+		setTitle("Goop of Death");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBackground(Color.black);
+		setVisible(true);
+		createBufferStrategy(3);
+		start();
+	}
+
+	private synchronized void start() {
+		running = true;
+		thread.start();
+	}
+
+	public synchronized void stop() {
+		running = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 
 	public static void main(String[] args) {
-		System.out.println("Wow such Game!!!");
+		Main game = new Main();
+	}
 
+	@Override
+	public void run() {
+		while (running) {
+			cam.update(world);
+			render();
+			if(focused != cam.focused){
+				if(cam.focused){
+					BufferedImage cursorImg = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
+					Cursor blankCursor = Toolkit.getDefaultToolkit().createCustomCursor(
+					    cursorImg, new Point(0, 0), "blank cursor");
+					getContentPane().setCursor(blankCursor);
+				}else{
+					getContentPane().setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+				}
+			}
+			try {
+				Thread.sleep(16);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void render() {
+		Graphics g = getBufferStrategy().getDrawGraphics();
+		g.drawImage(image, 0, 0, image.getWidth(), image.getHeight(), null);
+		getBufferStrategy().show();
 	}
 
 }
